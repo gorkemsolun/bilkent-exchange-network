a9229b32cf97fdc4474c70d0e251255c2ce566bc
Emir <tugluemir@gmail.com>
2023-09-28 23:16:43 +0300

    Add Second-hand Item Component #3
    SecondHandItem component can be used for lost&found, borrow, and donation items with minimal changes.

--- src/app/secondhand/page.tsx
+++ src/app/secondhand/page.tsx
@@ -1,12 +1,30 @@
 import SecondHandCategories from "./secondhandcategories";
+import SecondHandItem from "../../components/SecondHandItem"
 import "../../styles/globals.css";
 
 export default function SecondHand() {
+  const secondHandItems = [{  // dummy data
+    id: 1,
+    title: "tragedy of hamlet",
+    description: "desssscriptttttttttionnnnnnnnn offfffffffffff trrrrrrragedyyyyyyyyyyy offfffff hammmmmmlettttttttttt",
+    category: "Book",
+    imgSrc: "cs319.png",
+    price: "500"
+  },
+  {
+    id: 2,
+    title: "gilgamesh",
+    description: "desssscriptttttttttionnnnnnnnn offfffffffffff giiiiiiilgaaaaaaaaameshhhhhhhhhhhhhhhhh",
+    category: "Book",
+    imgSrc: "cs319.png",
+    price: "100"
+  }];
+
   return (
     <div className="flex flex-row grow">
       <SecondHandCategories></SecondHandCategories>
       <div className="w-full h-full">
-        <img className="" src="cs319.png" alt="cs319" />
+        <SecondHandItem secondHandItems={secondHandItems}/>
       </div>
     </div>
   );
