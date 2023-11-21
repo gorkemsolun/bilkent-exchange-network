52cfcbfc566155d84e7ae984306b11697c7c45ba
Emir <tugluemir@gmail.com>
2023-11-21 12:21:01 +0300

    Change Section Exchange Item #10

--- src/app/home/section-exchange/page.tsx
+++ src/app/home/section-exchange/page.tsx
@@ -1,26 +1,24 @@
-import SectionItem from "./sectionItem";
+import SectionItem from "./sectionitem";
 import "../../../styles/globals.css";
 import SearchBar from "../../../components/searchbar";
 import Categories from "../../../components/categories";
 
-export default function SecondHand() {
+export default function SectionExchange() {
   const sectionItems = [
     {
       // dummy data
       id: 1,
-      title: "Cs319 section 1",
-      description: "Eray Tüzün",
-      category: "Cs319",
-      imgSrc: "/cs319.png",
-      price: "33",
+      username: "Emir Tuğlu",
+      offeredSection: "CS 224-2",
+      desiredSection: "CS 224-1",
+      date: "21.11.2023 11.17",
     },
     {
-      id: 2,
-      title: "Cs202",
-      description: "Uğur Doğrusöz",
-      category: "Cs202",
-      imgSrc: "/cs319.png",
-      price: "222",
+      id: 1,
+      username: "Emir Tuğlu",
+      offeredSection: "CS 224-2",
+      desiredSection: "CS 224-1",
+      date: "21.11.2023 11.17",
     },
   ];
 
