d214f943f47a23a5989d306321030a0b51287490
ediz <c.edizcivan@gmail.com>
2023-12-08 15:19:33 +0300

    added logout functionality

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout"
 
 export default function Header() {
+  
+  const {logout} = useLogout()
+  
+  const handleClick = () =>{
+    logout()
+  }
+  
   return (
     <div className="bg-gradient-to-r from-blue-500 to-cyan-300 flex flex-row justify-center content-center items-center text-slate-800">
       <img className="w-20 p-2" src="logo.png" alt="Logo" />
       <Link className="p-4 text-2xl" to="/">
         Bilkent Exchange Network
       </Link>
+      <button onClick={handleClick}> Logout</button>
     </div>
   );
 }
---
+++ frontend/src/hooks/useLogout.jsx
@@ -0,0 +1,14 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+    const { dispatch } = useAuthContext()
+    const logout = () =>{
+        //remove user from storage
+        localStorage.removeItem('user')
+
+        // update auth context with logout
+       dispatch({type: 'LOGOUT'})
+       
+    }
+    return {logout}
+}
